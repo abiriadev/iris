1069f64266f5861e6e5444288687313524d53b6b
Abiria <abiria.dev@gmail.com>
2023-11-30 11:07:37 +0900

    docs: support reading arguments

--- examples/iriscat/main.go
+++ examples/iriscat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -11,11 +12,30 @@ import (
 )
 
 func main() {
-	stdinr := bufio.NewReader(os.Stdin)
+	flag.Parse()
+	f := flag.Arg(0)
+
+	var in io.Reader
+
+	if f == "" {
+		in = os.Stdin
+	} else {
+		var err error
+		in, err = os.Open(f)
+
+		fmt.Println(err)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	r := bufio.NewReader(in)
+	w := bufio.NewWriter(os.Stdout)
 	p := colorgrad.Sinebow()
 
-	for i := 0.0; ; i += 0.01 {
-		ch, _, err := stdinr.ReadRune()
+	for i, line := 0.0, 0.0; ; i += 0.01 {
+		ch, _, err := r.ReadRune()
 
 		if err == io.EOF {
 			break
@@ -25,6 +45,15 @@ func main() {
 			return
 		}
 
-		fmt.Printf("%s%c", iris.ColorFg(p.At(i)), ch)
+		if ch == '\n' {
+			line++
+			i = line / 100
+			w.WriteString(iris.Reset)
+			w.WriteRune(ch)
+			w.Flush()
+		} else {
+			w.WriteString(iris.ColorFg(p.At(i)))
+			w.WriteRune(ch)
+		}
 	}
 }
